c19d2a348a66e04b3d371fc966c87ac5159d387a
Natalia <164662428+Natalia-Honcharova@users.noreply.github.com>
2026-03-22 13:30:44 +0300

    hw-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,10 @@
+//Задача 2. Форматирование сообщений
 function formatMessage(message, maxLength) {
-
+    //решение
+    if (message.length <= maxLength) {
+        return message;
+    } 
+    return message.slice(0, maxLength) + "...";   
 }
 
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
